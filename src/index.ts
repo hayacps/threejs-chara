0c8479ed2f1863e9dbd877139c30e9b55b4c7e83
hayacps <hayato.abe@cps.akita-pu.ac.jp>
2024-06-25 17:46:08 +0900

    hello

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { CharacterControls } from './characterControls';
 import * as THREE from 'three'
 import { CameraHelper } from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 
 // SCENE
 const scene = new THREE.Scene();
